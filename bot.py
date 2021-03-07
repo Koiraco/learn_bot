740ff9a560b296eab6ca38ad51501383804dfd46
Koiraco <koiraco@gmail.com>
2021-03-07 18:24:02 +0300

    Added showing random cat photos by /cat command

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
+from glob import glob
 import logging
-from random import  randint
+from random import choice, randint
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -40,12 +41,19 @@ def guess_number(update, context):
         message = "Введите число"
     update.message.reply_text(message)
 
+def send_cat_picture(update, context):
+    cat_photo_list = glob('images/cat*jp*g')
+    cat_photo_filename = choice(cat_photo_list)
+    chat_id = update.effective_chat.id
+    context.bot.send_photo(chat_id=chat_id, photo=open(cat_photo_filename, 'rb'))
+
 def main():
     mybot = Updater(settings.API_KEY, use_context=True, request_kwargs=PROXY)
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
     dp.add_handler(CommandHandler("guess", guess_number))
+    dp.add_handler(CommandHandler("cat", send_cat_picture))
     dp.add_handler(MessageHandler(Filters.text, talk_to_me))
 
     logging.info("Бот стартовал!")
