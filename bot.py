338177c21b2d22503f13395a53543339d735e850
Koiraco <koiraco@gmail.com>
2021-03-16 16:55:38 +0300

    Added images and remembering user data

--- bot.py
+++ bot.py
@@ -12,19 +12,21 @@ PROXY = {'proxy_url': settings.PROXY_URL,
     'urllib3_proxy_kwargs': {'username': settings.PROXY_USERNAME, 'password': settings.PROXY_PASSWORD}}
 
 def greet_user(update, context):
-    smile = get_smile()
     print("Вызван /start")
-    update.message.reply_text(f"Здравствуй, пользователь {smile}!")
+    context.user_data['emoji'] = get_smile(context.user_data)
+    update.message.reply_text(f"Здравствуй, пользователь {context.user_data['emoji']}!")
 
 def talk_to_me(update, context):
-    smile = get_smile()
+    context.user_data['emoji'] = get_smile(context.user_data)
     text = update.message.text
     print(text)
-    update.message.reply_text(f"{text} {smile}")
+    update.message.reply_text(f"{text} {context.user_data['emoji']}")
 
-def get_smile():
-    smile = choice(settings.USER_EMOJI)
-    return emojize(smile, use_aliases=True)
+def get_smile(user_data):
+    if 'emoji' not in user_data:
+        smile = choice(settings.USER_EMOJI)
+        return emojize(smile, use_aliases=True)
+    return user_data['emoji']
 
 
 def play_random_numbers(user_number):
